3994bfe6107ac461b0ee2598ec66a4dbce98ed8f
bulyan <38100527+boelchan@users.noreply.github.com>
2025-05-08 09:25:04 +0700

    add models and migrations for Alasan and Permintaan

---
+++ database/migrations/2025_05_08_021949_create_permintaans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permintaans', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->text('perubahan');
+            $table->enum('status', ['baru', 'proses', 'selesai'])->default('baru');
+            $table->foreignId('alasan_id')->references('id')->on('alasans');
+            $table->integer('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permintaans');
+    }
+};
